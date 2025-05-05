86ca494e76201a630100fb459216a3f676f8455d
Rachit <rachitdua997@gmail.com>
2025-05-06 00:00:42 +0530

    Update Userdata.java

--- src/main/java/com/StudentData/TechWork/Entity/Userdata.java
+++ src/main/java/com/StudentData/TechWork/Entity/Userdata.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 public class Userdata {
 
-    @Id
+    @Id 
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
